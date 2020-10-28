78ea4433c84636ae86623bb607c7d9b3128f4931
Eric <bocherteric@gmail.com>
2020-10-28 11:44:38 +0000

    KNN added, Iob-Soc unsucessfully updated

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -10,18 +10,19 @@ int main()
 unsigned long long elapsed;
 unsigned int elapsedu;
 
-//timer_reset(TIMER_BASE);
+timer_reset(TIMER_BASE);
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\nHello world!\n");
   uart_txwait();
   //read current timer count, compute elapsed time
-timer_reset(TIMER_BASE);
+//timer_reset(TIMER_BASE);
 elapsed = timer_get_count(TIMER_BASE);
 elapsedu = timer_time_us(TIMER_BASE);
 
   uart_printf("\nExecution time: %d clocks in %dus @%dMHz\n\n",
   		(unsigned int)elapsed, elapsedu, FREQ/1000000);
+ //Latex ist besser als word
   uart_txwait();
   return 0;
   //char *a = malloc(10);
